d7aaf59171684e0a4afe1ed62a52117cd6058fe9
efojulius <awukujulius1@gmail.com>
2023-02-16 06:01:40 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/**
+/*
  *
  * main - main block
  * Return: o
  */
 int main(void)
 {
-	puts("\"programming is like developing a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
