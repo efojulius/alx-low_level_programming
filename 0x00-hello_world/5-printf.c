6c28e491816fb00914375f38c8a9bad9a8e837d7
efojulius <awukujulius1@gmail.com>
2023-02-16 06:37:03 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void) 
 {
-	printf("%s", "With proper grammar,but the outcome is a piece of art, \n");
+	printf("%s", "With proper grammar, but the outcome is a piece of art, \n");
 	return 0;
 }
