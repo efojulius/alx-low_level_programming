06cfb9f275db4610205cf4e30dad44237bbd8fb5
efojulius <awukujulius1@gmail.com>
2023-02-16 12:43:57 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
-int main(void) {
+int main(void) 
+{
     printf("Size of char: %lu byte(s)\n", sizeof(char));
     printf("Size of short: %lu byte(s)\n", sizeof(short));
     printf("Size of int: %lu byte(s)\n", sizeof(int));
