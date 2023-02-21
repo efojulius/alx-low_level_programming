fec3b545e802167f8535136d2d74a27b99224d07
efojulius <awukujulius1@gmail.com>
2023-02-21 15:26:23 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -24,7 +24,7 @@ int main(void)
 	fib2_half1 = fib2 / 1000000000;
 	fib1_half2 = fib1 % 1000000000;
 	fib2_half2 = fib2 % 1000000000;
-	for (count = 03; count < 99; count++)
+	for (count = 93; count < 99; count++)
 	{
 	half1 = fib1_half1 + fib2_half1;
 	half2 = fib1_half2 + fib2_half2;
